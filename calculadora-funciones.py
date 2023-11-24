ebbd908a451317f0e819c1f54db1ba27bc480b5d
crltsnch <crltsnch@myuax.com>
2023-11-24 17:05:09 +0100

    main

--- calculadora-funciones.py
+++ calculadora-funciones.py
@@ -29,6 +29,32 @@ def division(num1, num2):
     else:
         raise ValueError("No se puede dividir entre cero.")
 
-
+def main():
+    print("Bienvenido a la calculadora")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+    opcion = int(input("Ingrese la opción deseada: "))
+    if opcion == 5:
+        print("Gracias por usar la calculadora.")
+        return
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
+    if opcion == 1:
+        print("El resultado de la suma es: ", suma(num1, num2))
+    elif opcion == 2:
+        print("El resultado de la resta es: ", resta(num1, num2))
+    elif opcion == 3:
+        print("El resultado de la multiplicación es: ", multiplicacion(num1, num2))
+    elif opcion == 4:
+        try:
+            print("El resultado de la división es: ", division(num1, num2))
+        except ValueError as e:
+            print(e)
+    else:
+        print("Opción no soportada.")
     
-
+if __name__ == '__main__':
+    main()
