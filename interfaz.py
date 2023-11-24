2e8962e2691bb96c9dd003a541e009e5ebc588e7
crltsnch <crltsnch@myuax.com>
2023-11-24 17:32:10 +0100

    botones de operaciones

--- interfaz.py
+++ interfaz.py
@@ -22,4 +22,17 @@ class Interfaz:
         self.result_label = tk.Label(root, text="Resultado:")
         self.result_label.pack()
 
-        
+        self.button_suma = tk.Button(root, text="Suma", command=self.suma)
+        self.button_suma.pack()
+
+        self.button_resta = tk.Button(root, text="Resta", command=self.resta)
+        self.button_resta.pack()
+
+        self.button_multiplicacion = tk.Button(root, text="Multiplicación", command=self.multiplicacion)
+        self.button_multiplicacion.pack()
+
+        self.button_division = tk.Button(root, text="División", command=self.division)
+        self.button_division.pack()
+
+        self.quit_button = tk.Button(root, text="Salir", command=root.destroy)
+        self.quit_button.pack()
