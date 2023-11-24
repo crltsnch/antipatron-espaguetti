0e4957fcc2cc9d3cf525e0446798356c03b31e4e
crltsnch <crltsnch@myuax.com>
2023-11-24 17:22:23 +0100

    arrgelos

--- calculadora-funciones.py
+++ calculadora-funciones.py
@@ -14,9 +14,9 @@
         print("Operación no soportada.")'''
 
 class Calculadora:
-    def __init__(self):
-        self.num1 = 0
-        self.num2 = 0
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
     def suma(self):
         return self.num1 + self.num2
@@ -43,7 +43,10 @@ def menu():
     
 
 def main():
-    calculadora = Calculadora()
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    calculadora = Calculadora(num1, num2)
+
     while True:
         menu()
         opcion = int(input("Ingrese la opción deseada: "))
@@ -52,23 +55,19 @@ def main():
             print("Gracias por usar la calculadora.")
             break
 
-        num1 = float(input("Ingrese el primer número: "))
-        num2 = float(input("Ingrese el segundo número: "))
-
-        calculadora.set_numeros(num1, num2)
-
-        if opcion == 1:
-            resultado = calculadora.suma()
-        elif opcion == 2:
-            resultado = calculadora.resta()
-        elif opcion == 3:
-            resultado = calculadora.multiplicacion()
-        elif opcion == 4:
-            try:
-                resultado = calculadora.division()
-            except ValueError as e:
-                print(e)
-                continue
+        if opcion in [1, 2, 3, 4]:
+            if opcion == 1:
+                resultado = calculadora.suma()
+            elif opcion == 2:
+                resultado = calculadora.resta()
+            elif opcion == 3:
+                resultado = calculadora.multiplicacion()
+            elif opcion == 4:
+                try:
+                    resultado = calculadora.division()
+                except ValueError as e:
+                    print(e)
+                    continue
         else:
             print("Opción no soportada.")
             continue
