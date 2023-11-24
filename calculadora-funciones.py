7bb79eddbc0ebc49ba0d8a58eff3830f67d701f4
crltsnch <crltsnch@myuax.com>
2023-11-24 17:02:04 +0100

    division

--- calculadora-funciones.py
+++ calculadora-funciones.py
@@ -23,6 +23,12 @@ def resta(num1, num2):
 def multiplicacion(num1, num2):
     return num1 * num2
 
+def division(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        raise ValueError("No se puede dividir entre cero.")
+
 
     
 
